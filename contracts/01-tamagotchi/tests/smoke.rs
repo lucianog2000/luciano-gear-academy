97e7db0f68ca9cdefb18b5c03cdaba0d6beb5598
Luciano Emanuel García <legarcia2@teco.com.ar>
2024-01-24 19:42:53 -0300

    01-tamagotchi contract + tests

--- contracts/01-tamagotchi/tests/smoke.rs
+++ contracts/01-tamagotchi/tests/smoke.rs
@@ -1,10 +1,22 @@
-use gtest::{Program, System};
-
+use gtest::{Log, Program, System};
+use tamagotchi_io::{TmgAction, TmgEvent};
 #[test]
 fn smoke_test() {
+    // test contract initialization
     let sys = System::new();
     sys.init_logger();
-    let _program = Program::current(&sys);
+    let program = Program::current(&sys);
+    let res = program.send(2, String::from("Luchex"));
+    assert!(!res.main_failed());
 
-    // TODO: 8️⃣ Test the program initialization and message handling
+    // test handling name
+    let res = program.send(2, TmgAction::Name);
+    let expected_log = Log::builder()
+        .dest(2)
+        .payload(TmgEvent::Name("Luchex".to_string()));
+    assert!(res.contains(&expected_log));
+
+    // test handling age
+    let res = program.send(2, TmgAction::Age);
+    assert!(!res.main_failed());
 }
