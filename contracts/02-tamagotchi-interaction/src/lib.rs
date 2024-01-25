fce2bdc7d9dca24b673e5002fd2be8425f89a568
Luciano Emanuel García <legarcia2@teco.com.ar>
2024-01-25 08:50:30 -0300

    Copying the 01-tamagotchi files to the 02-tamagotchi-interaction folder

--- contracts/02-tamagotchi-interaction/src/lib.rs
+++ contracts/02-tamagotchi-interaction/src/lib.rs
@@ -1,22 +1,59 @@
 #![no_std]
 
 #[allow(unused_imports)]
-use gstd::prelude::*;
+use gstd::{debug, exec, msg, prelude::*};
+use tamagotchi_io::{Tamagotchi, TmgAction};
 
+static mut TAMAGOTCHI: Option<Tamagotchi> = None;
 // TODO: 4️⃣ Define constants
 
 #[no_mangle]
 extern fn init() {
     // TODO: 0️⃣ Copy the `init` function from the previous lesson and push changes to the master branch
+    let tamagotchi = Tamagotchi {
+        name: msg::load().expect("Can't decode tamagotchi's name"),
+        date_of_birth: exec::block_timestamp(),
+    };
+    debug!(
+        "The Tamagotchi Program was initialized with name {:?} and birth date {:?}",
+        tamagotchi.name, tamagotchi.date_of_birth
+    );
+    unsafe { TAMAGOTCHI = Some(tamagotchi) };
 }
 
 #[no_mangle]
 extern fn handle() {
     // TODO: 0️⃣ Copy the `handle` function from the previous lesson and push changes to the master branch
+    let tamagotchi = unsafe {
+        TAMAGOTCHI
+            .as_mut()
+            .expect("The contract is not initialized")
+    };
+
+    let name = &tamagotchi.name;
+    let current_time = exec::block_timestamp();
+    let age = current_time - tamagotchi.date_of_birth;
+    let tmg_action: TmgAction = msg::load().expect("Error loading TmgAction");
+    match tmg_action {
+        TmgAction::Name => {
+            debug!("Message: Name");
+            msg::reply(name, 0).expect("Error in sending tamagotchi's name");
+        }
+        TmgAction::Age => {
+            debug!("Message: Age");
+            msg::reply(age.to_string(), 0).expect("Error in sending tamagotchi's age");
+        }
+    }
     // TODO: 5️⃣ Add new logic for calculating the `fed`, `entertained` and `slept` levels
 }
 
 #[no_mangle]
 extern fn state() {
     // TODO: 0️⃣ Copy the `handle` function from the previous lesson and push changes to the master branch
+    let tamagotchi = unsafe {
+        TAMAGOTCHI
+            .as_ref()
+            .expect("The contract is not initialized")
+    };
+    msg::reply(tamagotchi, 0).expect("Failed to share state");
 }
--- contracts/02-tamagotchi-interaction/tests/interaction.rs
+++ contracts/02-tamagotchi-interaction/tests/interaction.rs
@@ -3,6 +3,21 @@ use gtest::{Program, System};
 // TODO: 0️⃣ Copy tests from the previous lesson and push changes to the master branch
 
 #[test]
+fn smoke_test() {
+    // test contract initialization
+    let sys = System::new();
+    sys.init_logger();
+    let program = Program::current(&sys);
+    let res = program.send(2, String::from("Luchex"));
+    assert!(!res.main_failed());
+
+    // test handling name
+    let res = program.send(2, TmgAction::Name);
+    let expected_log = Log::builder().dest(2).payload(String::from("Luchex"));
+    assert!(res.contains(&expected_log));
+}
+
+#[test]
 fn interaction_test() {
     let sys = System::new();
     sys.init_logger();
