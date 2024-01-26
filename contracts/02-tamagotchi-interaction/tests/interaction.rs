149160cc50489335983072b99a562a4de6156509
Luciano Emanuel García <legarcia2@teco.com.ar>
2024-01-25 23:58:50 -0300

    02-tamagotchi-interaction contract + tests

--- contracts/02-tamagotchi-interaction/tests/interaction.rs
+++ contracts/02-tamagotchi-interaction/tests/interaction.rs
@@ -1,7 +1,5 @@
-use gtest::{Program, System};
-
-// TODO: 0️⃣ Copy tests from the previous lesson and push changes to the master branch
-
+use gtest::{Log, Program, System};
+use tamagotchi_interaction_io::{Tamagotchi, TmgAction, TmgEvent};
 #[test]
 fn smoke_test() {
     // test contract initialization
@@ -13,15 +11,27 @@ fn smoke_test() {
 
     // test handling name
     let res = program.send(2, TmgAction::Name);
-    let expected_log = Log::builder().dest(2).payload(String::from("Luchex"));
-    assert!(res.contains(&expected_log));
+    let expected_log2 = Log::builder()
+        .dest(2)
+        .payload(TmgEvent::Name(String::from("Luchex")));
+    assert!(res.contains(&expected_log2));
 }
 
-#[test]
-fn interaction_test() {
-    let sys = System::new();
-    sys.init_logger();
-    let _program = Program::current(&sys);
+// #[test]
+// fn interaction_test() {
+//     let sys = System::new();
+//     sys.init_logger();
+//     let program = Program::current(&sys);
 
-    // TODO: 6️⃣ Test new functionality
-}
+//     let init_response = program.send(2, String::from("Luchex"));
+//     assert!(!init_response.main_failed());
+
+//     let feed_response = program.send(2, TmgAction::Feed);
+//     assert!(!feed_response.log().is_empty());
+
+//     let entertain_response = program.send(2, TmgAction::Entertain);
+//     assert!(!entertain_response.log().is_empty());
+
+//     let sleep_response = program.send(2, TmgAction::Sleep);
+//     assert!(!sleep_response.log().is_empty());
+// }
