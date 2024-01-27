274bd5b84b58d095c1ad883a857f359a8fd6b227
Luciano Emanuel García <legarcia2@teco.com.ar>
2024-01-27 12:53:17 -0300

    Copying the 02-tamagotchi-interaction files to the 03-tamagotchi-nft folder

--- contracts/03-tamagotchi-nft/tests/owning.rs
+++ contracts/03-tamagotchi-nft/tests/owning.rs
@@ -1,6 +1,38 @@
-use gtest::{Program, System};
+use gtest::{Log, Program, System};
+use tamagotchi_interaction_io::{Tamagotchi, TmgAction};
+#[test]
+fn smoke_test() {
+    // test contract initialization
+    let sys = System::new();
+    sys.init_logger();
+    let program = Program::current(&sys);
+    let res = program.send(2, String::from("Luchex"));
+    assert!(!res.main_failed());
 
-// TODO: 0️⃣ Copy tests from the previous lesson and push changes to the master branch
+    // test handling name
+    let res = program.send(2, TmgAction::Name);
+    let expected_log = Log::builder().dest(2).payload(String::from("Luchex"));
+    assert!(res.contains(&expected_log));
+}
+
+#[test]
+fn interaction_test() {
+    let sys = System::new();
+    sys.init_logger();
+    let program = Program::current(&sys);
+
+    let init_response = program.send(2, String::from("Luchex"));
+    assert!(!init_response.main_failed());
+
+    let feed_response = program.send(2, TmgAction::Feed);
+    assert!(!feed_response.log().is_empty());
+
+    let entertain_response = program.send(2, TmgAction::Entertain);
+    assert!(!entertain_response.log().is_empty());
+
+    let sleep_response = program.send(2, TmgAction::Sleep);
+    assert!(!sleep_response.log().is_empty());
+}
 
 #[test]
 fn owning_test() {
