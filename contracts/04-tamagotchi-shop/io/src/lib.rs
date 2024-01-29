925c862e6747c86f729de0fe0523fe84c0e6adaa
Luciano Emanuel García <legarcia2@teco.com.ar>
2024-01-29 14:41:51 -0300

    04-tamagotchi-shop contract

--- contracts/04-tamagotchi-shop/io/src/lib.rs
+++ contracts/04-tamagotchi-shop/io/src/lib.rs
@@ -4,6 +4,7 @@ use codec::{Decode, Encode};
 use gmeta::{In, InOut, Metadata, Out};
 use gstd::{prelude::*, ActorId};
 use scale_info::TypeInfo;
+use store_io::{AttributeId, TransactionId};
 
 #[derive(Default, Encode, Decode, TypeInfo)]
 #[codec(crate = gstd::codec)]
@@ -20,6 +21,9 @@ pub struct Tamagotchi {
     pub slept: u32,
     pub slept_block: u32,
     pub approved_account: Option<ActorId>,
+    pub ft_contract_id: Option<ActorId>,
+    pub transaction_id: u64,
+    pub approve_transaction: Option<(TransactionId, ActorId, u128)>,
 }
 
 #[derive(Encode, Decode, TypeInfo)]
@@ -34,6 +38,15 @@ pub enum TmgAction {
     Transfer(ActorId),
     Approve(ActorId),
     RevokeApproval,
+    SetFTokenContract(ActorId),
+    ApproveTokens {
+        account: ActorId,
+        amount: u128,
+    },
+    BuyAttribute {
+        store_id: ActorId,
+        attribute_id: AttributeId,
+    },
 }
 
 #[derive(Encode, Decode, TypeInfo)]
@@ -49,6 +62,12 @@ pub enum TmgEvent {
     Transferred(ActorId),
     Approved(ActorId),
     ApprovalRevoked,
+    FTokenContractSet,
+    TokensApproved { account: ActorId, amount: u128 },
+    ApprovalError,
+    AttributeBought(AttributeId),
+    CompletePrevPurchase(AttributeId),
+    ErrorDuringPurchase,
 }
 
 pub struct ProgramMetadata;
