f7bd46ca94e4cc1041c8264a960478fe27918520
Luciano Emanuel García <legarcia2@teco.com.ar>
2024-01-30 19:43:51 -0300

    Copying 05-tamagotchi-auto files to 06-tamagotchi-army folder

--- contracts/06-tamagotchi-army/io/src/lib.rs
+++ contracts/06-tamagotchi-army/io/src/lib.rs
@@ -1,39 +1,93 @@
 #![no_std]
-
-use gmeta::Metadata;
-use gstd::prelude::*;
+use gmeta::{In, InOut, Metadata, Out};
+use gstd::{prelude::*, ActorId, ReservationId};
+use store_io::{AttributeId, TransactionId};
 
 #[derive(Default, Encode, Decode, TypeInfo)]
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
 pub struct Tamagotchi {
-    // TODO: 0️⃣ Copy fields from previous lesson and push changes to the master branch
-    // TODO: 1️⃣ Add new fields
+    pub name: String,
+    pub date_of_birth: u64,
+    pub owner: ActorId,
+    pub fed: u32,
+    pub fed_block: u32,
+    pub entertained: u32,
+    pub entertained_block: u32,
+    pub slept: u32,
+    pub slept_block: u32,
+    pub approved_account: Option<ActorId>,
+    pub ft_contract_id: Option<ActorId>,
+    pub transaction_id: u64,
+    pub approve_transaction: Option<(TransactionId, ActorId, u128)>,
+    pub reservations: Vec<ReservationId>,
 }
 
 #[derive(Encode, Decode, TypeInfo)]
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
 pub enum TmgAction {
-    // TODO: 0️⃣ Copy actions from previous lesson and push changes to the master branch
-    // TODO: 2️⃣ Add new actions
+    Name,
+    Age,
+    Feed,
+    Entertain,
+    Sleep,
+    Transfer(ActorId),
+    Approve(ActorId),
+    RevokeApproval,
+    SetFTokenContract(ActorId),
+    ApproveTokens {
+        account: ActorId,
+        amount: u128,
+    },
+    BuyAttribute {
+        store_id: ActorId,
+        attribute_id: AttributeId,
+    },
+    CheckState,
+    ReserveGas {
+        reservation_amount: u64,
+        duration: u32,
+    },
 }
 
 #[derive(Encode, Decode, TypeInfo)]
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
 pub enum TmgEvent {
-    // TODO: 0️⃣ Copy events from previous lesson and push changes to the master branch
-    // TODO: 3️⃣ Add new events
+    Name(String),
+    Age(u64),
+    Fed,
+    Entertained,
+    Slept,
+    Transferred(ActorId),
+    Approved(ActorId),
+    ApprovalRevoked,
+    FTokenContractSet,
+    TokensApproved { account: ActorId, amount: u128 },
+    ApprovalError,
+    AttributeBought(AttributeId),
+    CompletePrevPurchase(AttributeId),
+    ErrorDuringPurchase,
+    FeedMe,
+    PlayWithMe,
+    WantToSleep,
+    NothingToDo,
+    MakeReservation,
+    GasReserved,
+}
+
+pub struct GasReservationHandler {
+    pub contract_send_a_delayed_message: bool,
+    pub can_send_delayed_message: bool,
 }
 
 pub struct ProgramMetadata;
 
-// TODO: 0️⃣ Copy `Metadata` from the first lesson and push changes to the master branch
 impl Metadata for ProgramMetadata {
-    type Init = ();
-    type Handle = ();
-    type State = ();
+    type Init = In<String>;
+    type Handle = InOut<TmgAction, TmgEvent>;
+    type State = Out<Tamagotchi>;
     type Reply = ();
     type Others = ();
     type Signal = ();
